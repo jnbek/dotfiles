e0fc4049e3fbb52d1ab28719f604eed5011f6212
John D Jones III <jnbek1972@gmail.com>
2010-03-06 15:16:09 -0700

    Fix the installer to backup and better handle the .vim dir

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+# Usage: ./install.sh [--no-bak]
+# --no-bak erases the .vim/ directory backup, if it exists as a directory.
+
+if [ -d ${HOME}/.vim/ ]; then
+    mv ${HOME}/.vim $HOME/.vim.bak;
+fi;
 for i in _*
 do 
     source="${PWD}/$i"
     target="${HOME}/${i/_/.}"
-    echo "${target} already exists"       
     ln -fivs ${source} ${target}
 done
-
+if [ "$1" = "--no-bak" ]; then
+    rm -ir ${HOME}/.vim.bak
+fi;
