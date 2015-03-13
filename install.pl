cbda331e4f64b2e374093a0cde12e00b7125725d
John D Jones III <jnbek1972@gmail.com>
2015-03-13 16:51:03 -0600

    Stop making backup directories if there's no need

--- install.pl
+++ install.pl
@@ -9,15 +9,13 @@ use Env qw(HOME USER PATH);
 use File::Path qw(make_path);
 use Term::ANSIColor qw(:constants);
 
-our $VERSION = "1.6";
+our $VERSION = "1.7";
 ( bless {}, __PACKAGE__ )->main();
 
 sub main {
     my $self = shift;
-    my $now  = time;
-    my $cwd  = $self->{'cwd'} ||= do { getcwd(); };
     $self->{'bak_path'} = "$cwd/backups/$USER/$now";
-    make_path( $self->{'bak_path'}, { verbose => 1 } );
+    $self->{'cwd'}      = getcwd();
     my $cpc = $self->do_cpanp_conf();
     my $cnt = map { $self->install($_) } ( glob "_*" );
     return 0;
@@ -27,12 +25,14 @@ sub install {
     my $self = shift;
     my $name = shift;
     my $cwd  = $self->{'cwd'};
+    my $now  = time;
     my $orig = $name;
     $name =~ s/^_/\./xms;
     #$name =~ s/\.sample$//xm if $name =~ m/\.sample$/mx;
     if (   ( -f "$HOME/$name" && !-l "$HOME/$name" )
         || ( -d "$HOME/$name" && !-l "$HOME/$name" ) )
     {
+        make_path( $self->{'bak_path'}, { verbose => 1 } );
         move( "$HOME/$name", $self->{'bak_path'} . "/$name" );
     }
     if ( -l "$HOME/$name" ) {
@@ -47,7 +47,10 @@ sub install {
 
 sub do_cpanp_conf {
     my $self = shift;
-    return unless $self->which("cpanp");
+    unless ( $self->which("cpanp") ) {    #Sniff $PATH for cpanp and bail if not present
+        print RED, "Skipping cpanplus configuration: cpanp not found!!\n", RESET;
+        return;
+    }
     my $cwd  = $self->{'cwd'};
     my $name = "User.pm";
     my $targ = "$HOME/.cpanplus/lib/CPANPLUS/Config/$name";
@@ -58,7 +61,7 @@ sub do_cpanp_conf {
     if ( -f $targ && !-l $targ ) {
         move( $targ, $self->{'bak_path'} . "/$name" );
     }
-    if ( -l $orig ) {
+    if ( -l $targ ) {
         print BOLD, YELLOW, "Skipping $orig -> $targ: Symlink exists\n", RESET;
     }
     else {
@@ -74,5 +77,6 @@ sub which {
     foreach my $dir ( split ':', $PATH ) {
         return "$dir/$program" if -x "$dir/$program";
     }
+    return;
 }
 
