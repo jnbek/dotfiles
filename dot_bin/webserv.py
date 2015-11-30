ca3663733b6b2de9261824fdcafe393e7a49848d
John D Jones III <jnbek1972@gmail.com>
2015-11-30 12:38:24 -0700

    Add webserv script and fix serve_dir alias

---
+++ dot_bin/webserv.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Launch small http server
+
+Works on both Python 2 and 3
+"""
+
+import sys
+
+def default_port():
+    return 4000
+
+def main():
+    try:
+        from SimpleHTTPServer import SimpleHTTPRequestHandler
+    except ImportError:
+        from http.server import SimpleHTTPRequestHandler
+
+    try:
+        from SocketServer import TCPServer as HTTPServer
+    except ImportError:
+        from http.server import HTTPServer
+
+    # simple web server
+    # serves files relative to the current directory.
+
+    try:
+        server_port = int(sys.argv[1])
+    except:
+        server_port = default_port()
+
+    httpd = HTTPServer(("", server_port), SimpleHTTPRequestHandler)
+    print("serving at port {0}".format(server_port))
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    main()
+
